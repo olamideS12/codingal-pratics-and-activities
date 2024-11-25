81c70c1409aa0e418adcfa687f5ff10409275805
olamideS12 <haddiyataffeez2@gmail.com>
2024-11-25 19:55:11 +0100

    js

---
+++ java script/script.js
@@ -0,0 +1,19 @@
+function add(){
+    var a =6;
+    var b =15
+    var c = a +b 
+    document.getElementById("p1").innerHTML="addition is "+ c;
+}
+function sub(){
+    var a =6;
+    var b =15
+    var c = a -b 
+    document.getElementById("p1").innerHTML="subtraction is "+ c;
+}
+
+function multiplication(){
+    var a =6;
+    var b =15
+    var c = a *b 
+    document.getElementById("p1").innerHTML="multiplication is "+ c;
+}
